ab227ded22094bec076c95dbb580e0d318a673f8
munaciella <franes88@gmail.com>
2025-03-07 20:36:26 +0000

    fix type error in actions folder

--- src/app/dashboard/files/[id]/page.tsx
+++ src/app/dashboard/files/[id]/page.tsx
@@ -1,11 +1,11 @@
-const ChatToFilePage = ({ params }: { params: { id: string } }) => {
-    const originalWarn = console.warn;
-    console.warn = (message, ...args) => {
-      if (message.includes("params should be awaited")) return; // ✅ Ignore specific warning
-      originalWarn(message, ...args); // ✅ Keep other warnings
-    };
-  
-    return <div>ChatToFilePage: {params.id}</div>;
+const ChatToFilePage = ({
+  params: { id },
+}: {
+  params: {
+    id: string;
   };
-  
-  export default ChatToFilePage;
+}) => {
+  return <div>ChatToFilePage: {id}</div>;
+};
+
+export default ChatToFilePage;
